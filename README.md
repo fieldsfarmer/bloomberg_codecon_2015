bef0d255dfb7e5551ae150b7e87f733292beee44
fieldsfarmer <fieldsfarmer@gmail.com>
2016-10-12 14:30:37 -0400

    match gloves

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 bloomberg codecon 2015
+[LINK](http://codecon.bloomberg.com/2015-finals)
---
+++ matching_gloves.py
@@ -0,0 +1,36 @@
+import sys
+import collections
+
+def matching_gloves(arr):
+	d = collections.defaultdict(int)
+	for a in arr:
+		if isParlindrome(a):
+			continue
+		d[a]+=1
+	res = 0
+	for a in d.keys():
+		t = a[::-1]
+		if t in d and d[t]>0 and d[t] == d[a]:
+			res += d[t]
+			d[t] = 0
+	if res == 0:
+		for a in arr:
+			if not isParlindrome(a):
+				return -1
+		return 0
+	return res
+
+
+
+def isParlindrome(a):
+	t = a[::-1]
+	return t == a
+
+def main():
+	# print(isParlindrome('ioi'))
+	arr = ['bcd', 'erty', 'ytre', 'opipo', 'dcb']
+	arr = ['abcde', 'edcba', 'abcde']
+	arr = ['ioi', 'ertre', 'ghhg']
+	print(matching_gloves(arr))
+if __name__ == '__main__':
+	main()
