c1c636534f66fcb48a0b37008c88f872d2e8c806
fieldsfarmer <fieldsfarmer@gmail.com>
2016-10-12 20:49:17 -0400

    conference room 2

--- conference_room_scheduler.py
+++ conference_room_scheduler.py
@@ -7,14 +7,14 @@ class TimeRoom(object):
 	def book(self, room_id, time_slot, n):
 		if room_id < 1 or room_id > self.total:
 			return False
-		if time_slot < 1 or time_slot > 32 or (time_slot+n)>32:
+		if time_slot < 1 or time_slot > 32 or (time_slot+n-1)>32:
 			return False
 		res = False
-		for i in range(time_slot, time_slot+n+1):
+		for i in range(time_slot, time_slot+n):
 			if self.table[room_id][i]:
 				return res
 		res = True
-		for i in range(time_slot, time_slot+n+1):
+		for i in range(time_slot, time_slot+n):
 			self.table[room_id][i] = True
 		return res
 
@@ -24,7 +24,7 @@ class TimeRoom(object):
 			return res
 		for i in range(1,self.total+1):
 			booked = False
-			for j in range(time_slot, time_slot+n+1):
+			for j in range(time_slot, time_slot+n):
 				if self.table[i][j]:
 					booked = True
 			if not booked:
@@ -32,47 +32,41 @@ class TimeRoom(object):
 		return res
 
 
+# data = sys.stdin.read().splitlines()
+# total = None
+# res = []
+# for line in data :
+#     if not total:
+#         total = int(line)
+#     else:
+#         res.append(line)
+        
+res = ['1-1-4', '1-2']
+total = 3
 
-def main():
-	res = []
-	res = ['1-1-4', '1-2']
-	# total_rooms = int(input())
-	total_rooms = 3
+tr = TimeRoom(total)
 
-	# for l in sys.stdin:
-	# 	res.append(l.strip())
-
-	tr = TimeRoom(total_rooms)
-
-	output = []
-	for q in res:
-		arr = q.split('-')
-		arr = list(map(int, arr))
-		# print(arr)
-		if len(arr) == 3:
-			if tr.book(*arr):
-				output.append('Y')
-			else:
-				output.append('N')
-		elif len(arr) == 2:
-			t = tr.query(*arr)
-			if not t:
-				output.append('')
-			else:
-				output.append(t)
-	for r in output:
-		if isinstance(r, list):
-			for i in r:
-				print(i, end=" ")
+output = []
+for q in res:
+	arr = q.split('-')
+	arr = list(map(int, arr))
+	# print(arr)
+	if len(arr) == 3:
+		if tr.book(*arr):
+			output.append('Y')
 		else:
-			print(r)
+			output.append('N')
+	elif len(arr) == 2:
+		t = tr.query(*arr)
+		if not t:
+			output.append('None')
+		else:
+			output.append(t)
 
-
-	# for i in range(time_slot, time_slot+n+1):
-	# line = sys.stdin.readline().strip()
-	# while line:
-	# 	res.append(int(line))
-	# 	line = sys.stdin.readline().strip()
-	# print(res)
-if __name__ == '__main__':
-	main()
+for r in output:
+	if isinstance(r, list):
+		for u in r:
+			print(u, end=" ")
+		print()
+	else:
+		print(r)
