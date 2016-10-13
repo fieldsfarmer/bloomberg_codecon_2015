890d13b2d706ddd6799dcb11bc232c7ef9975fcd
fieldsfarmer <fieldsfarmer@gmail.com>
2016-10-12 21:48:25 -0400

    matching gloves 2

--- matching_gloves.py
+++ matching_gloves.py
@@ -1,42 +1,60 @@
+# A good C++ version using unordered_multiset
+# https://github.com/kdungs/codecon/blob/master/src/gloves.cc
+import sys
 import collections
 
 def matching_gloves(arr):
-	d = collections.defaultdict(int)
-	for a in arr:
-		if isParlindrome(a):
-			continue
-		d[a]+=1
-	res = 0
-	for a in d.keys():
-		t = a[::-1]
-		if t in d and d[t]>0 and d[t] == d[a]:
-			res += d[t]
-			d[t] = 0
-	if res == 0:
-		for a in arr:
-			if not isParlindrome(a):
-				return -1
-		return 0
-	return res
+    res = 0
+    d = dict()
+    for a in arr:
+        u = a[::-1]
+        if a == u:
+            continue
+        if u in d:
+            d[u]-=1
+            if d[u] == 0:
+                d.pop(u)
+            res += 1
+        else:
+            if a not in d:
+                d[a] = 1
+            else:
+                d[a] += 1
+    if len(d)==0:
+        return res
+    else:
+        return -1
+
+def matching_gloves1(arr):
+    res = 0
+    d = collections.defaultdict(int)
+    for a in arr:
+        u = a[::-1]
+        if a == u:
+            continue
+        if u in d:
+            d[u]-=1
+            if d[u] == 0:
+                d.pop(u)
+            res += 1
+        else:
+            d[a]+=1
+    if len(d)==0:
+        return res
+    else:
+        return -1
 
 
+# data = sys.stdin.read().splitlines()
+# sz = None
+# arr = []
+# for line in data :
+#     if not sz:
+#         sz = int(line)
+#     else:
+#         arr.append(line)
 
-def isParlindrome(a):
-	t = a[::-1]
-	return t == a
-
-def main():
-	# print(isParlindrome('ioi'))
-	arr = ['bcd', 'erty', 'ytre', 'opipo', 'dcb']
-	arr = ['abcde', 'edcba', 'abcde']
-	arr = ['ioi', 'ertre', 'ghhg']
-	print(matching_gloves(arr))
-
-	# sz = int(input())
-	# arr = []
-	# for i in range(sz):
-	# 	arr.append(input())
-	# print(matching_gloves(arr))
-	
-if __name__ == '__main__':
-	main()
+arr = ['bcd', 'erty', 'ytre', 'opipo', 'dcb']
+# arr = ['abcde', 'edcba', 'abcde']
+# arr = ['ioi', 'ertre', 'ghhg']
+print(matching_gloves1(arr))
